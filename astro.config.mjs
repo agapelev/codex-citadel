fc8fa3685681bdddb9468be977a8456a69cb0ce1
Lev Dessyatykh <dessyatykh@gmail.com>
2026-03-05 19:23:30 +0500

    fix: Изменена главная страница, применены стили TAILWIND

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,23 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Docs with Tailwind',
+			title: 'Кодекс «Цитадели Духа»',
+			// Вот это — ПРАВИЛЬНЫЙ путь инъекции футера:
+			components: {
+				Footer: './src/components/CustomFooter.astro',
+			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/agapelev/codex-citadel' }],
 			sidebar: [
 				{
 					label: 'Руководства',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+			items: [{ label: 'Руководство по обучению', slug: 'guides/example' }],
 				},
 				{
 					label: 'Справочники',
-					autogenerate: { directory: 'reference' },
+			autogenerate: { directory: 'reference' },
 				},
 			],
 			customCss: ['./src/styles/global.css'],
